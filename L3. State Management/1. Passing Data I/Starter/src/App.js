9802f061480eb3b460bd3198c8f718b40b43b60d
Claudia Hill <hillc255@yahoo.com>
2023-09-29 07:50:20 -0400

    Update exercisE

--- L3. State Management/1. Passing Data I/Starter/src/App.js
+++ L3. State Management/1. Passing Data I/Starter/src/App.js
@@ -100,9 +100,22 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
-      <h2>Favorite Movies</h2>
+      <ul>
+        <h2>Favorite Movies</h2>
+
+          {profiles.map((profile) => {
+          const userName = users[profile.userID].name;
+          const favMovieName = movies[profile.favoriteMovieID].name;
+
+          return(
+            <li key={profile.id}>
+              <p>{`${userName}'s favorite movie is "${favMovieName}."`}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  );
-};
+    );     
+  };
 
 export default App;
